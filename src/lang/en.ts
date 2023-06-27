3dbb12cdb888ec49c84b6baa7002cc2a5990553f
Aziz Kızgın <azizkzgn@gmail.com>
2023-06-27 19:53:09 +0300

    feat: localization files added

---
+++ src/lang/en.ts
@@ -0,0 +1,9 @@
+export const en = {
+  appTitle: 'manage courses',
+  signIn: 'sign In',
+  forgotPassword: 'Forgot Your Password? ',
+  resetPassword: 'Reset Password',
+  invalid_email: 'Invalid email',
+  email_is_required: 'Email is required',
+  password_is_required: 'Password is required',
+};
---
+++ src/lang/tr.ts
@@ -0,0 +1,9 @@
+export const tr = {
+  appTitle: 'manage courses',
+  signIn: 'Giriş Yap',
+  forgotPassword: 'Şifrenizi mi unuttunuz? ',
+  resetPassword: 'Şifreyi Sıfırla',
+  invalid_email: 'Geçersiz e-posta',
+  email_is_required: 'E-posta gereklidir',
+  password_is_required: 'Şifre gereklidir',
+};
