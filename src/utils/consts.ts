979586584e4bc46703885262c3944f5ca40a90b7
Aziz Kızgın <azizkzgn@gmail.com>
2023-06-27 22:30:13 +0300

    feat: new images added

---
+++ src/utils/consts.ts
@@ -0,0 +1,6 @@
+import {ImageSourcePropType} from 'react-native';
+
+export const mortarBoard: ImageSourcePropType = require('../assets/icons/mortarboard.png');
+export const mortarBoardOutline: ImageSourcePropType = require('../assets/icons/mortarboard-outline.png');
+export const turkeyFlag: ImageSourcePropType = require('../assets/icons/turkey.png');
+export const ukFlag: ImageSourcePropType = require('../assets/icons/uk.png');
