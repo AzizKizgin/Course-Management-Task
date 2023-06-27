8f7ef24943382aa230b4c545d94aa5b185ace2cb
Aziz Kızgın <azizkzgn@gmail.com>
2023-06-27 13:45:06 +0300

    feat: libraries added

--- App.tsx
+++ App.tsx
@@ -1,15 +1,26 @@
+import {NavigationContainer} from '@react-navigation/native';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {Text, Box, NativeBaseProvider} from 'native-base';
 import React from 'react';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const App = () => {
   const queryClient = new QueryClient();
   return (
     <NativeBaseProvider>
       <QueryClientProvider client={queryClient}>
-        <Box>
-          <Text>Open up App.tsx to start working on your app!</Text>
-        </Box>
+        <NavigationContainer>
+          <SafeAreaView
+            style={{
+              flex: 1,
+              backgroundColor: 'white',
+            }}>
+            <Box flex={1} background={'#2234a8'}>
+              <Ionicons name="person" size={21} color={'#6d1f66'} />
+            </Box>
+          </SafeAreaView>
+        </NavigationContainer>
       </QueryClientProvider>
     </NativeBaseProvider>
   );
