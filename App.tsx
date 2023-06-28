bc12600d8c2da63121b154e61f3af323bde9f3fc
Aziz Kızgın <azizkzgn@gmail.com>
2023-06-29 00:31:12 +0300

    feat: app updated

--- App.tsx
+++ App.tsx
@@ -2,25 +2,28 @@ import {NavigationContainer} from '@react-navigation/native';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {Text, Box, NativeBaseProvider} from 'native-base';
 import React from 'react';
-import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import theme from './src/config/theme';
+import Login from './src/screens/Login';
+import {LocalizationProvider} from './src/contexts/LocalizationContext';
+import MainNavigation from './src/navigations/MainNavigation';
+import {UserProvider} from './src/contexts/UserContext';
 
 const App = () => {
+  const config = {
+    dependencies: {
+      'linear-gradient': require('react-native-linear-gradient').default,
+    },
+  };
   const queryClient = new QueryClient();
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={theme} config={config}>
       <QueryClientProvider client={queryClient}>
-        <NavigationContainer>
-          <SafeAreaView
-            style={{
-              flex: 1,
-              backgroundColor: 'white',
-            }}>
-            <Box flex={1} background={'#2234a8'}>
-              <Ionicons name="person" size={21} color={'#6d1f66'} />
-            </Box>
-          </SafeAreaView>
-        </NavigationContainer>
+        <LocalizationProvider>
+          <UserProvider>
+            <MainNavigation />
+          </UserProvider>
+        </LocalizationProvider>
       </QueryClientProvider>
     </NativeBaseProvider>
   );
