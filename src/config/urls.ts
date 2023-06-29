654538bdd6ac4329808dd287c48961c529e6068b
Aziz Kızgın <azizkzgn@gmail.com>
2023-06-29 19:53:34 +0300

    feat: urls added

---
+++ src/config/urls.ts
@@ -0,0 +1,7 @@
+const baseUrl = 'https://dummyjson.com';
+
+export const urls = {
+  getAllStudents: `${baseUrl}/users`,
+  searchStudents: `${baseUrl}/search`,
+  addStudents: `${baseUrl}/users/add`,
+};
