a6cb68c4bccbcfb4c51cf7dc6004a3c745bab425
Aziz Kızgın <azizkzgn@gmail.com>
2023-06-30 22:48:18 +0300

    feat: useStudentSchema added

--- src/config/validation.ts
+++ src/config/validation.ts
@@ -5,3 +5,13 @@ export const useLoginSchema = () =>
     email: Yup.string().email('invalid_email').required('email_is_required'),
     password: Yup.string().required('password_is_required'),
   });
+
+export const useStudentSchema = () =>
+  Yup.object().shape({
+    firstName: Yup.string().required('first_name_is_required'),
+    lastName: Yup.string().required('last_name_is_required'),
+    email: Yup.string().email('invalid_email').required('email_is_required'),
+    phone: Yup.string().required('phone_is_required'),
+    username: Yup.string().required('username_is_required'),
+    companyName: Yup.string().required('company_name_is_required'),
+  });
