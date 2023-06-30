bebb7fbb11f85fb3753a0deb3c685fd30f1d89ae
Aziz Kızgın <azizkzgn@gmail.com>
2023-06-30 20:14:38 +0300

    feat: api functions added

--- src/screens/Students/components/StudentItem.tsx
+++ src/screens/Students/components/StudentItem.tsx
@@ -1,4 +1,4 @@
-import React, {FC, memo} from 'react';
+import React, {FC, memo, useCallback} from 'react';
 import {
   Card,
   HStack,
@@ -12,7 +12,13 @@ import {
 } from 'native-base';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {getItemColor} from '../../../utils/helpers';
-import {Student} from '../../../types/types';
+import {Student, StudentNavigationParams} from '../../../types/types';
+import {NavigationProp, useNavigation} from '@react-navigation/native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import {useMutation, useQueryClient} from '@tanstack/react-query';
+import {deleteStudents} from '../../../utils/api';
+import {Alert} from 'react-native';
+import {useLocalization} from '../../../contexts/LocalizationContext';
 
 interface StudentItemProps {
   student: Student;
@@ -20,8 +26,47 @@ interface StudentItemProps {
 }
 
 const StudentItem: FC<StudentItemProps> = ({student, index}) => {
+  const {strings} = useLocalization();
+  const queryCache = useQueryClient();
+  const navigation = useNavigation<NavigationProp<StudentNavigationParams>>();
+  const onPress = () => {
+    navigation.navigate('StudentDetail', {
+      studentId: student.id,
+    });
+  };
+
+  const {mutate: deleteStudentMutate, isLoading} = useMutation({
+    mutationFn: deleteStudents,
+    onSuccess: () => {
+      Alert.alert(strings.success, strings.student_deleted_successfully);
+      queryCache.invalidateQueries(['students']);
+    },
+    onError: () => {
+      Alert.alert(strings.error, strings.something_went_wrong);
+    },
+  });
+
+  const onDelete = useCallback(() => {
+    Alert.alert(strings.delete, strings.are_you_sure_want_to_delete_student, [
+      {
+        text: strings.yes,
+        onPress: () => {
+          deleteStudentMutate(student.id);
+        },
+      },
+      {
+        text: strings.no,
+      },
+    ]);
+  }, [strings, deleteStudentMutate, student.id]);
+
   return (
-    <Pressable onPress={() => console.log('Pressed')} shadow={2}>
+    <Pressable
+      onPress={onPress}
+      shadow={2}
+      _pressed={{
+        shadow: 4,
+      }}>
       <Card
         key={student.id}
         backgroundColor={getItemColor(index)}
@@ -47,18 +92,29 @@ const StudentItem: FC<StudentItemProps> = ({student, index}) => {
             <Text fontSize="md" color={theme.colors.black}>
               {student.username}
             </Text>
-            <Text fontSize="sm" color={theme.colors.black}>
+            <Text fontSize="sm" color={theme.colors.black} numberOfLines={1}>
               {student.email}
             </Text>
           </VStack>
-          <Box alignItems="center" justifyContent="center" alignSelf="center">
+          <HStack
+            alignItems="center"
+            justifyContent="center"
+            alignSelf="center">
+            <Pressable onPress={onDelete}>
+              <Icon
+                as={MaterialCommunityIcons}
+                name="delete-outline"
+                size={7}
+                color={theme.colors.black}
+              />
+            </Pressable>
             <Icon
               as={Ionicons}
               name="chevron-forward-outline"
               size={6}
               color={theme.colors.black}
             />
-          </Box>
+          </HStack>
         </HStack>
       </Card>
     </Pressable>
