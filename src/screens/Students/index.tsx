a2176a7437f0f3b64d51e62c311eae46f668b8fd
Aziz Kızgın <azizkzgn@gmail.com>
2023-06-30 20:13:20 +0300

    feat: screen updated

--- src/screens/Students/index.tsx
+++ src/screens/Students/index.tsx
@@ -1,29 +1,33 @@
 import React, {useEffect, useState} from 'react';
-import {StudentResponse, Student} from '../../types/types';
-import {Box, Center} from 'native-base';
+import {Box, Center, HStack} from 'native-base';
 import theme from '../../config/theme';
 import {Text} from 'native-base';
 import {FlatList} from 'react-native-gesture-handler';
 import StudentItem from './components/StudentItem';
-import SearchBar from './components/SearchBar';
-import {useQuery} from '@tanstack/react-query';
-import {getAllStudents} from '../../utils/api';
+import SearchBar from '../../components/shared/SearchBar';
 import {ActivityIndicator, RefreshControl} from 'react-native';
 import {useLocalization} from '../../contexts/LocalizationContext';
 import ListFooter from './components/ListFooter';
 
+import {useQuery} from '@tanstack/react-query';
+import {StudentResponse} from '../../types/types';
+import {searchStudents} from '../../utils/api';
+import AddButton from '../../components/shared/AddButton';
+import {useDebounce} from 'use-debounce';
+
 const Students = () => {
   const {strings} = useLocalization();
   const [limit, setLimit] = useState(5);
-  const [page, setPage] = useState(1);
-
+  const [page, setPage] = useState(0);
+  const [search, setSearch] = useState('');
+  const [debouncedQuery] = useDebounce(search, 400);
   const {
     data: students,
     isLoading,
     isFetching,
     refetch,
-  } = useQuery<StudentResponse>(['students'], () => {
-    return getAllStudents(limit, page * limit);
+  } = useQuery<StudentResponse>(['students', debouncedQuery], () => {
+    return searchStudents(debouncedQuery, limit, page * limit);
   });
 
   useEffect(() => {
@@ -32,6 +36,15 @@ const Students = () => {
 
   return (
     <Box flex={1} bg="white">
+      <HStack
+        space={3}
+        width="90%"
+        alignSelf="center"
+        alignItems="center"
+        justifyContent="space-between">
+        <SearchBar query={search} setQuery={setSearch} />
+        <AddButton />
+      </HStack>
       {isLoading || isFetching ? (
         <Center flex={1}>
           <Text fontSize="sm">{strings.loading}</Text>
@@ -54,13 +67,13 @@ const Students = () => {
           renderItem={({item, index}) => (
             <StudentItem student={item} index={index} />
           )}
-          ListHeaderComponent={() => <SearchBar />}
           ListFooterComponent={() => (
             <ListFooter
               limit={limit}
               setLimit={setLimit}
               page={page}
               setPage={setPage}
+              total={students?.total || 0}
             />
           )}
           keyExtractor={item => item.id.toString()}
